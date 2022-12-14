4ca70ad69670145cf77f86762e9bcd1cedef902c
Faldiansyah <faldiansyahk@gmail.com>
2022-12-14 19:20:31 +0800

    Update layout

--- app/src/main/java/com/binar/c5team/gotraveladmin/MainActivity.kt
+++ app/src/main/java/com/binar/c5team/gotraveladmin/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_coba)
+        setContentView(R.layout.fragment_percobaan_layout)
     }
 }
